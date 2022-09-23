d5b56b422800e5c229870a97dc56ec9c696a674d
NguyenThiTuyetNganB1909954 <tuyetngan241tn@gmail.com>
2022-09-23 12:19:18 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors")
 const ContactsRouter = require("./app/routes/contact.route");
+const ApiError = require("./app/api-error");
+
 
 const app = express();
 
@@ -11,5 +13,16 @@ app.use("/api/contacts", ContactsRouter);
 app.get('/',(req,res) => {
     res.json({msg:"Welcome to contact book application."})
 });
+// middleware
+
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+});
+app.use((error, req, res, next) => {
+    return res.status(error.statusCode || 500).json({
+        msg: error.message || "Internal Server Error"
+    });
+});
+
 
 module.exports = app;
