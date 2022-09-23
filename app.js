5d579d196b9f8275932598a0486b662151c4279d
NguyenThiTuyetNganB1909954 <tuyetngan241tn@gmail.com>
2022-09-23 12:11:34 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const cors = require("cors")
+const ContactsRouter = require("./app/routes/contact.route");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/contacts", ContactsRouter);
 
 app.get('/',(req,res) => {
     res.json({msg:"Welcome to contact book application."})
---
+++ app/controllers/contact.controller.js
@@ -0,0 +1,27 @@
+exports.create = (req,res) => {
+    res.send({ msg: "create handler"});
+};
+
+exports.findAll = (req,res) => {
+    res.send({ msg: "create findAll"});
+};
+
+exports.findOne = (req,res) => {
+    res.send({ msg: "create findOne"});
+};
+
+exports.update = (req,res) => {
+    res.send({ msg: "create update"});
+};
+
+exports.delete = (req,res) => {
+    res.send({ msg: "create delete"});
+};
+
+exports.deleteAll = (req,res) => {
+    res.send({ msg: "create deleteAll"});
+};
+
+exports.findAllFavorite = (req,res) => {
+    res.send({ msg: "create findAllFavorite"});
+};
